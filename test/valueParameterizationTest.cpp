98d4f9c3a5aa78237c9e966b93a804390391fa6a
nwm1cob <Anootha.Ismail@bosch.com>
2023-07-27 12:40:40 +0530

    testfixture_paramete

--- test/valueParameterizationTest.cpp
+++ test/valueParameterizationTest.cpp
@@ -1,37 +1,67 @@
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 
-class DivFunctionTestSuite:public testing::TestWithParam<std::tuple<int,int,int>> {
-protected:
-DivFunctionTestSuite(){}
-~DivFunctionTestSuite(){}
 
-int divFun(int numerator, int denominator){
-    if(denominator==0 || denominator<0 ){return 0;}
+
+
+int divFunc(int numerator, int denominator){
+
+    if(denominator <= 0){
+
+        return 0;
+
+    }
+
     return numerator/denominator;
 
 }
+
+
+
+
+class DivFuncTestSuite:public testing::TestWithParam<std::tuple<int, int, int>>{
+
+protected:
+
+    DivFuncTestSuite(){}
+
+    ~DivFuncTestSuite(){}
+
 };
 
-TEST_P(DivFunTestSuite,HandleValidInputs){
-int numerator =std::get<0>(GetParam());
-int denominator =std::get<1>(GetParam());
-int expectedValue=std::get<2>(GetParam());
-int actualValue=divFun(numerator, denominator);
-ASSERT_EQ(actualValue, expectedValue);
+
+
+
+TEST_P(DivFuncTestSuite, HandleInputs){
+
+    int numerator = std::get<0>(GetParam());
+
+    int denominator = std::get<1>(GetParam());
+
+    int expectedValue = std::get<2>(GetParam());
+
+    int actualValue = divFunc(numerator, denominator);
+
+    ASSERT_EQ(actualValue, expectedValue);
+
 }
 
-INSTANTIATE_TEST_SUITE_P(DivFunTestSuiteParameterName, DivFunctionTestSuite,
- ::testing::values(
-    std::make_tuple(10,5,2),
-    std::make_tuple(10,0,0),
-    std::make_tuple(10,-5,0))
- );
 
-/*
-TEST(DivFunTestSuite,HandleNegetiveDenominator){
-int numerator =std::get<0>(GetParam());
-int denominator =std::get<1>(GetParam());
-int expectedValue=std::get<2>(GetParam());
-int actualValue=divFun(numerator, denominator);
-ASSERT_EQ(actualValue, expectedValue);
-}*/
+
+
+INSTANTIATE_TEST_SUITE_P(
+
+    DivFuncTestSuiteParameterExample,
+
+    DivFuncTestSuite,
+
+    ::testing::Values(
+
+        std::make_tuple(10, 5, 20),
+
+        std::make_tuple(10, 0, 0),
+
+        std::make_tuple(10, -5, 0)
+
+    )
+
+);
