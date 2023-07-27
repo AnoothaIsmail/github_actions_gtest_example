3fe2a2c91bc4c5df47c6e3a754474c50d758416f
nwm1cob <Anootha.Ismail@bosch.com>
2023-07-27 11:10:54 +0530

    testfixture

---
+++ test/fixtureExample.cpp
@@ -0,0 +1,42 @@
+
+
+class C{};
+class B{
+    C* ptr;
+    public:
+    B(C* addressOfObject) : ptr(addressOfObject) { //B depends on C pointer
+
+    }
+};
+class A{
+    B* ptr;
+    public:
+    A(B* addressOfObject) : ptr(addressOfObject) {//A depends on C pointer
+
+    }
+    bool operation(){
+        return true;
+    }
+};
+
+
+
+TEST("TestFixtureTestSuite", "OperationsTrueTest"){
+    //Arrange
+    C cObj;
+    B bObj(&cObj);
+    A aObj(&bObj);
+    //Act and Assert
+    ASSERT_FALSE(obj.operation());
+
+}
+
+TEST("TestFixtureTestSuite", "OperationsFalseTest"){
+    //Arrange
+    C cObj;
+    B bObj(&cObj);
+    A aObj(&bObj);
+    //Act and Assert
+    ASSERT_TRUE(obj.operation());
+
+}
