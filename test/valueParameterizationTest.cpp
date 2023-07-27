b17be134956d445c9e62a0a86ea3911357c9d9c3
nwm1cob <Anootha.Ismail@bosch.com>
2023-07-27 12:09:41 +0530

    testfixture_paramete

--- test/valueParameterizationTest.cpp
+++ test/valueParameterizationTest.cpp
@@ -1,3 +1,4 @@
+#include "gtest/gtest.h"
 int divFun(int numerator, int denominator){
     if(denominator==0 || denominator<0 ){return 0;}
     return numerator/denominator;
@@ -14,7 +15,7 @@ ASSERT_EQ(actualValue, expectedValue);
 TEST(DivFunTestSuite,HandleNegetiveDenominator){
 int numerator = 10;
 int denominator =-5;
-int expectedValue=2;
+int expectedValue=-2;
 int actualValue=divFun(numerator, denominator);
 ASSERT_EQ(actualValue, expectedValue);
 }
