e04c33c0e3118ca1101a3b8827494f98ce9ecb73
nwm1cob <Anootha.Ismail@bosch.com>
2023-07-27 11:46:22 +0530

    testfixture

--- test/fixtureExampleTest.cpp
+++ test/fixtureExampleTest.cpp
@@ -2,18 +2,10 @@
 #include "gtest/gtest.h"
 
 class C{};
-
 class B{
-
     C* ptr;
-
     public:
-
     B(C* addressOfObject): ptr{addressOfObject}{
-
-
-
-
     }
 
 };
@@ -22,61 +14,79 @@ class B{
 
 
 class A{
-
     B* ptr;
-
     public:
-
     A(B* addressOfObject): ptr{addressOfObject}{
-
-
-
-
     }
 
     bool  operation(){
-
         return true;
-
     }
 
 };
 
 
 
-
+/*
 TEST(TestFixtureTestSuite,OperationTrueTest){
-
     //Arrange
-
     C cObj;
-
     B bObj(&cObj);
-
     A obj(&bObj);
-
     //Act and Assert
-
     ASSERT_TRUE(obj.operation());
 
 }
 
 TEST(TestFixtureTestSuite,OperationFalseTest){
-
     //Arrange
-
     C cObj;
-
     B bObj(&cObj);
-
     A obj(&bObj);
-
     //Act and Assert
-
     ASSERT_FALSE(obj.operation());
 
+} */
 
+class TestFixtureTestSuite:public testing::Test{
+protected:
+A * aPtr;
+B* bPtr;
+C* cPtr;
+TestFixtureTestSuite(){
+    //Initialize
+}
+~TestFixtureTestSuite(){
+    //Releasing the resources
+}
+void SetUp(){
+    //Arrange
+    this->cPtr=new C();
+    this->bPtr=new B(cPtr);
+    this->aPtr=new A(bPtr);
 
+}
+void TearDown(){
+    delete cPtr;
+    delete bPtr;
+    delete aPtr;
+//delete resources
+}
 
+};
+
+TEST_F(TestFixtureTestSuite,OperationTrueTest){
+    //Arrange
+   
+    //Act and Assert
+    ASSERT_TRUE(aPtr->operation());
+
+}
+
+TEST_F(TestFixtureTestSuite,OperationFalseTest){
+    //Arrange
+   
+    //Act and Assert
+    ASSERT_TRUE(aPtr->operation());
 
 }
