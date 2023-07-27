ca3ac4ed34875c49e901eab5fbc45bbc9be8ff55
nwm1cob <Anootha.Ismail@bosch.com>
2023-07-27 12:32:04 +0530

    testfixture_paramete

--- test/valueParameterizationTest.cpp
+++ test/valueParameterizationTest.cpp
@@ -1,21 +1,35 @@
 #include "gtest/gtest.h"
+
+class DivFunctionTestSuite:public testing::TestWithParam<std::tuple<int,int,int>> {
+protected:
+DivFunctionTestSuite(){}
+~DivFunctionTestSuite(){}
+}
 int divFun(int numerator, int denominator){
     if(denominator==0 || denominator<0 ){return 0;}
     return numerator/denominator;
 
 }
-TEST(DivFunTestSuite,HandleValidInputs){
-int numerator = 10;
-int denominator =5;
-int expectedValue=2;
+TEST_P(DivFunTestSuite,HandleValidInputs){
+int numerator =std::get<0>(GetParam());
+int denominator =std::get<1>(GetParam());
+int expectedValue=std::get<2>(GetParam());
 int actualValue=divFun(numerator, denominator);
 ASSERT_EQ(actualValue, expectedValue);
 }
 
+INSTANTIATE_TEST_SUITE_P(DivFunTestSuiteParameterName, DivFunctionTestSuite,
+ ::testing::values(
+    std::make_tuple(10,5,2),
+    std::make_tuple(10,0,0),
+    std::make_tuple(10,-5,0))
+ );
+
+/*
 TEST(DivFunTestSuite,HandleNegetiveDenominator){
-int numerator = 10;
-int denominator =-5;
-int expectedValue=-2;
+int numerator =std::get<0>(GetParam());
+int denominator =std::get<1>(GetParam());
+int expectedValue=std::get<2>(GetParam());
 int actualValue=divFun(numerator, denominator);
 ASSERT_EQ(actualValue, expectedValue);
-}
+}*/
